3e68dc0fc1933ab189fb27aaf5f3e12abae8146e
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2020-12-13 10:46:52 +0600

    adding set Notification Sound for local push notification

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/NotificationChannel.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/notification/NotificationChannel.java
@@ -1,7 +1,6 @@
 package com.wix.reactnativenotifications.core.notification;
 
-//import android.app.NotificationManager;
-import androidx.core.app.NotificationManagerCompat as NotificationManager;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.graphics.Color;
 import android.media.RingtoneManager;
